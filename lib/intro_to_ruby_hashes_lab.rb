5a2049b674c22aa2815af51745824573b1d51ccc
plony benav <menachemfuterfas@gmail.com>
2020-11-12 17:35:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,18 @@
 def new_hash
-  new = {}
+   empty_hash = {}
 end
 
 def my_hash
-  new_Hash = {:random_val => "random"}
+
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  new_Hash = {:name => 'Grace Hopper'
+
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  new_Hash = {}
-  new_Hash[:id] = number
+
 end
