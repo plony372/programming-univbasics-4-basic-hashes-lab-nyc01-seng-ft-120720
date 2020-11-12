5727d7feb26bccdae73564e1568699c3a7bfc043
plony benav <menachemfuterfas@gmail.com>
2020-11-12 17:30:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,18 @@
 def new_hash
-  Hash.new
+  new_Hash = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  new_Hash = {:random_val => "random"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_Hash = {:name => 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  
 end
