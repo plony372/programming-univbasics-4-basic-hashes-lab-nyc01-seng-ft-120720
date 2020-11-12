163b2fab6fa0f6bf65c250e0b948de545d863c4e
plony benav <menachemfuterfas@gmail.com>
2020-11-12 17:49:36 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,20 @@
 def new_hash
-  # return an empty hash
+   empty_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  valid_hash = {:valid => "hashman"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  symbal_hash = {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  grades = {:id => number}
+
+  
 end
